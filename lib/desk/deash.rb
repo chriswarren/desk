3f6a956dcefb6841f299cf0d968bed71f47d598e
Colin Calvert <ccalvert@nvows.com>
2013-07-24 17:53:40 -0500

    Fixed access to some of the case items. Cleaned up a bit.

--- lib/desk/deash.rb
+++ lib/desk/deash.rb
@@ -17,22 +17,42 @@ module Hashie
   end
 
   class Deash < Mash
+    # Object#type is deprecated 
+    Mash.send :undef_method, :type
+
+    def each
+      if includes_key_chain?("raw._embedded.entries")
+        self.raw._embedded['entries'].each do |entry|
+          yield entry
+        end
+      else
+        super
+      end
+    end
 
     def method_missing(method, *args, &block)
       return self.[](method) if key?(method)
+      # TODO: Make this DRY
       if includes_key_chain?("_links."+method.to_s)
         return nil if !self._links[method]
         return Desk.get(self._links[method].href.sub("/api/#{self.version}/", ""))
       elsif includes_key_chain?("raw._links."+method.to_s)
         return nil if !self.raw._links[method]
         return Desk.get(self.raw._links[method].href.sub("/api/#{self.version}/", ""))
+      elsif includes_key_chain?("raw."+method.to_s)
+        return nil if !self.raw[method]
+        return self.raw[method]
       end
       return super
     end
 
-    def links
-      Links.new(self._links) if key?("_links")
-      Links.new(self.raw._links) if includes_key_chain?("raw._links")
+    def id
+      if includes_key_chain?("raw._links.self.href")
+        case self._links.self['class']
+        when 'case', 'phone_call'
+          self._links.self.href.split("/")[4].to_i
+        end
+      end
     end
 
     def includes_key_chain?(chain)
@@ -44,14 +64,9 @@ module Hashie
       true
     end
 
-    def each
-      if includes_key_chain?("raw._embedded.entries")
-        self.raw._embedded['entries'].each do |entry|
-          yield entry
-        end
-      else
-        super
-      end
+    def links
+      Links.new(self._links) if key?("_links")
+      Links.new(self.raw._links) if includes_key_chain?("raw._links")
     end
 
     def results
