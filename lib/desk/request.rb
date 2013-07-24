70052653eee17d8e239c925eeb9ba414af94818a
Colin Calvert <ccalvert@nvows.com>
2013-07-24 11:05:38 -0500

    Moved REQUEST_METHODS to a constant. Test REQUEST_METHODS in respond_to for deashie to use.

--- lib/desk/request.rb
+++ lib/desk/request.rb
@@ -2,14 +2,28 @@ module Desk
   # Defines HTTP request methods
   module Request
     require 'json' unless defined?(::JSON)
+    REQUEST_METHODS = [
+      'get',
+      'patch',
+      'post',
+      'put',
+      'delete'
+    ].freeze
 
     def method_missing(method_name, *args, &block)
-      request_methods = ['get','patch','post','put','delete']
-      if (request_methods.include? method_name.to_s) && (args.length > 0)
+      if (REQUEST_METHODS.include? method_name.to_s) && (args.length > 0)
         path = args[0]
         options = args[1] ? args[1] : {}
         raw = args[2] ? args[2] : false
         request(method_name.to_sym, path, options, raw)
+      else
+        super
+      end
+    end
+
+    def respond_to?(method_name, include_private = false)
+      if (REQUEST_METHODS.include? method_name.to_s)
+        true
       else
         super
       end
