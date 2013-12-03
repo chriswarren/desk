53858aac33c2a9711437d410e84ada0953217ccd
Colin Calvert <ccalvert@nvows.com>
2013-12-03 17:12:14 -0600

    Fixed override method names to match automatic method generation structure

--- lib/desk/client/case.rb
+++ lib/desk/client/case.rb
@@ -5,7 +5,6 @@ module Desk
 
       def case_endpoints
         [ :list, :search, :create, :update, 
-          :list_message,
           :list_replies, :show_reply, :create_reply, :update_reply,
           :list_notes, :show_note, :create_note,
           :list_attachments, :show_attachment, :create_attachment, :delete_attachment,
@@ -20,43 +19,48 @@ module Desk
       end
       alias_method :case, :show_case
 
-      def list_message_attachments(case_id, *args)
+      def show_case_message(case_id)
+        get("cases/#{case_id}/message")
+      end
+      alias_method :case_message, :show_case_message
+
+      def list_case_message_attachments(case_id, *args)
         options = args.last.is_a?(Hash) ? args.pop : {}
         get("cases/#{case_id}/message/attachments", options)
       end
-      alias_method :message_attachments, :list_message_attachments
+      alias_method :case_message_attachments, :list_case_message_attachments
 
-      def list_reply_attachments(case_id, reply_id, *args)
+      def list_case_reply_attachments(case_id, reply_id, *args)
         options = args.last.is_a?(Hash) ? args.pop : {}
         get("cases/#{case_id}/replies/#{reply_id}/attachments", options)
       end
-      alias_method :reply_attachments, :list_reply_attachments
+      alias_method :case_reply_attachments, :list_case_reply_attachments
 
-      def show_message_attachment(case_id, attachment_id)
+      def show_case_message_attachment(case_id, attachment_id)
         get("cases/#{case_id}/message/attachments/#{attachment_id}")
       end
-      alias_method :message_attachment, :show_message_attachment
+      alias_method :case_message_attachment, :show_case_message_attachment
 
-      def show_reply_attachment(case_id, reply_id, attachment_id)
+      def show_case_reply_attachment(case_id, reply_id, attachment_id)
         get("cases/#{case_id}/replies/#{reply_id}/attachments/#{attachment_id}")
       end
-      alias_method :reply_attachment, :show_reply_attachment
+      alias_method :case_reply_attachment, :show_case_reply_attachment
 
-      def create_message_attachment(case_id, *args)
+      def create_case_message_attachment(case_id, *args)
         options = args.last.is_a?(Hash) ? args.pop : {}
         post("cases/#{case_id}/message/attachments", options)
       end
 
-      def create_reply_attachment(case_id, reply_id, *args)
+      def create_case_reply_attachment(case_id, reply_id, *args)
         options = args.last.is_a?(Hash) ? args.pop : {}
         post("cases/#{case_id}/replies/#{reply_id}/attachments", options)
       end
 
-      def delete_message_attachment(case_id, attachment_id)
+      def delete_case_message_attachment(case_id, attachment_id)
         delete("cases/#{case_id}/message/attachments/#{attachment_id}")
       end
 
-      def delete_reply_attachment(case_id, reply_id, attachment_id)
+      def delete_case_reply_attachment(case_id, reply_id, attachment_id)
         delete("cases/#{case_id}/replies/#{reply_id}/attachments/#{attachment_id}")
       end
 
