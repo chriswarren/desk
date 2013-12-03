283b917befcdf3d009e1743f7ae6d9712bab9d96
Colin Calvert <ccalvert@nvows.com>
2013-12-03 13:18:01 -0600

    Added custom id support for system message endpoint

--- lib/desk/client/system_message.rb
+++ lib/desk/client/system_message.rb
@@ -7,6 +7,10 @@ module Desk
       end
       alias_method :system_message, :show_system_message
 
+      def system_message_id(href, parent_id = false)
+        nil
+      end
+
     end
   end
 end
