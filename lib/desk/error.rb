418a8361cb9fe6a1cb722aa5998463c9cf96ca83
Alex Dean <alex@snowplowanalytics.com>
2014-03-27 19:30:07 +0200

    Added missing -s as per @colinc's comment https://github.com/chriswarren/desk/issues/29#issuecomment-38822520

--- lib/desk/error.rb
+++ lib/desk/error.rb
@@ -10,15 +10,15 @@ module Desk
     end
 
     def ratelimit_reset
-      Time.at(@http_headers.values_at('x-ratelimit-reset', 'X-RateLimit-Reset').detect {|value| value }.to_i)
+      Time.at(@http_headers.values_at('x-rate-limit-reset', 'X-Rate-Limit-Reset').detect {|value| value }.to_i)
     end
 
     def ratelimit_limit
-      @http_headers.values_at('x-ratelimit-limit', 'X-RateLimit-Limit').detect {|value| value }.to_i
+      @http_headers.values_at('x-rate-limit-limit', 'X-Rate-Limit-Limit').detect {|value| value }.to_i
     end
 
     def ratelimit_remaining
-      @http_headers.values_at('x-ratelimit-remaining', 'X-RateLimit-Remaining').detect {|value| value }.to_i
+      @http_headers.values_at('x-rate-limit-remaining', 'X-Rate-Limit-Remaining').detect {|value| value }.to_i
     end
 
     def retry_after
