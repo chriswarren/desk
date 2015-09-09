4f590f4e00b72c8f0eeef081c9536fb4b8fb8423
Afonso Tsukamoto <atsukamoto@faber-ventures.com>
2015-09-09 11:52:48 +0100

    Added test for threaded config.

--- spec/desk/api_spec.rb
+++ spec/desk/api_spec.rb
@@ -45,6 +45,23 @@ describe Desk::API do
           :version => "amazing",
           :logger => double('logger')
         }
+
+        @alternative_configuration = {
+          :consumer_key => 'Louie',
+          :consumer_secret => 'CounterStrike',
+          :oauth_token => 'plOT',
+          :oauth_token_secret => 'OperatingSystem',
+          :adapter => :sueohpyt,
+          :format => :json,
+          :max_requests => 5,
+          :proxy => 'http://tsuk:public@proxy.example.com:8080',
+          :subdomain => 'stresscoder',
+          :support_email => 'problem@stresscoder.com',
+          :use_max_requests => false,
+          :user_agent => 'Generic User Agent',
+          :version => "boring",
+          :logger => double('logger')
+        }
       end
 
       context "during initialization"
@@ -66,6 +83,54 @@ describe Desk::API do
           @keys.each do |key|
             api.send(key).should == @configuration[key]
           end
+        end
+
+        it 'should keep different configurations for each thread' do
+          # config on current thread
+          api_config = ->{
+            api = Desk::API.new
+            @configuration.each do |key, value|
+              api.send("#{key}=", value)
+            end
+            api
+          }
+          api = api_config.call()
+
+          # get mutexes to ensure right execution steps
+          m1 = Mutex.new # will block main thread until thread config done
+          m2 = Mutex.new # will block thread until main thread asserts
+          m2.lock
+
+          # launch a new thread with new config
+          t = Thread.new do
+            m1.lock # make main thread wait for thread config
+            api2 = Desk::API.new
+            @alternative_configuration.each do |key, value|
+              api2.send("#{key}=", value)
+            end
+            m1.unlock
+
+            # Wait for main thread to finish assertions and reconfig
+            m2.synchronize{
+              @alternative_configuration.each do |key, value|
+                api2.send("#{key}").should eq(value)
+              end
+            }
+          end
+
+          # Wait for new thread to finish contfig
+          m1.synchronize{
+            # check if current thread keeps config
+            @configuration.each do |key, value|
+              api.send("#{key}").should eq(value)
+            end
+
+            # reconfig to test if thread changes
+            api = api_config.call()
+
+            m2.unlock # allow new thread to assert
+          }
+          t.join
         end
       end
     end
