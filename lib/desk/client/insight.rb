bef2a58ec06b0cb4534b29d099b70a1205abee84
Cb-James <Cb-James@users.noreply.github.com>
2016-05-05 13:59:44 -0700

    support for insights v3

--- lib/desk/client/insight.rb
+++ lib/desk/client/insight.rb
@@ -3,13 +3,13 @@ module Desk
     module Insight
 
       def show_insights_meta
-        get("insights/meta")
+        get("insights3/meta")
       end
       alias_method :insights_meta, :show_insights_meta
 
       def create_insights_report(*args)
         options = args.last.is_a?(Hash) ? args.pop : {}
-        post("insights/reports", options)
+        post("insights3/reports", options)
       end
 
     end
